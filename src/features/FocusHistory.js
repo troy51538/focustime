4d0df249b7adde296c91868f103db37891cd0e6a
joon <90282370+troy51538@users.noreply.github.com>
2024-05-29 22:02:01 -0400

    first commit

---
+++ src/features/FocusHistory.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
+
+export const FocusHistory = ({ history }) => {
+  if (!history || !history.length) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>We haven't focused on anything yet</Text>
+      </View>
+    );
+  }
+
+  const renderItem = ({ item }) => {
+    return <Text style={styles.item}>- {item}</Text>;
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focused on</Text>
+      <FlatList data={history} renderItem={renderItem} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: spacing.md,
+    flex: 1,
+  },
+  title: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+  item: {
+    fontSize: fontSizes.md,
+    color: colors.white,
+  },
+});
